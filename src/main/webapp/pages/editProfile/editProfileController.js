492409a5ea1150d3119fc95678e5efe0ec4a66e8
Eivydas Senkus <eivyses@gmail.com>
2017-04-30 13:07:08 +0300

    test

--- src/main/webapp/pages/editProfile/editProfileController.js
+++ src/main/webapp/pages/editProfile/editProfileController.js
@@ -2,7 +2,6 @@ var app = angular.module("myApp");
 
 app.controller('editProfileController', ['$scope', '$rootScope', '$cookieStore', '$location', function($scope, $rootScope, $cookieStore, $location) {
 	
-	console.log("Error");
 	
 	$scope.email = $rootScope.profile.email;
 	
